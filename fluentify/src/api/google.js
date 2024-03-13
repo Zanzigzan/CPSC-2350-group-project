1af4ea22a8b6fed79682b5f1db55b313d77a0a2e
Pawel Michalski <pawel230401@gmail.com>
2024-03-13 16:03:14 -0700

    Getting parameters  and returning result in translate function implemented #1

--- fluentify/src/api/google.js
+++ fluentify/src/api/google.js
@@ -2,29 +2,41 @@
 const apiUrl = import.meta.env.VITE_GOOGLE_URL;
 const apiKey = import.meta.env.VITE_GOOGLE_KEY;
 
-// function to translate given text
-export async function Translate() {
-    const data = { // TODO
-        q: "Hello World!",
-        target: "fr"
+// The function to translate given text
+export async function Translate(text, target) {
+    const data = {
+        "q": text,
+        "target": target
     };
 
-    const response = await fetch(`${apiUrl}?key=${apiKey}`, {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-        'Content-Type': 'application/json',
-        },
-    });
+    try {
+        const response = await fetch(`${apiUrl}?key=${apiKey}`, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
 
-    const result = await response.json();
+        const result = await response.json();
 
-    if (response.ok) {
-      // TODO
-      console.log(":)");
-      console.log(result.data.translations[0].translatedText);
-    } else {
-      // TODO
-      console.log("T-T");
+        if (response.ok) {
+            console.log(result.data.translations[0]);
+
+            return {
+                "detectedSourceLanguage": result.data.translations[0].detectedSourceLanguage,
+                "translatedText": result.data.translations[0].translatedText
+            }
+        } else {
+            // Handle HTTP errors
+            console.error("HTTP Error:", response.status, response.statusText);
+
+            throw new Error(`HTTP Error: ${response.status} ${response.statusText}`);
+        }
+    } catch (error) {
+        // Handle other errors
+        console.error("Error:", error.message);
+        
+        throw error;
     }
 }
