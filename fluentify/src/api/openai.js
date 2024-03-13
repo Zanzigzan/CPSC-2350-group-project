47110ed32d09cac607302f71334b9b8edd81b7de
Pawel Michalski <pawel230401@gmail.com>
2024-03-13 16:56:00 -0700

    Prompt env variables added to generateText function #4

--- fluentify/src/api/openai.js
+++ fluentify/src/api/openai.js
@@ -2,6 +2,10 @@
 const apiUrl = import.meta.env.OPENAI_URL;
 const apiKey = import.meta.env.OPENAI_KEY;
 
+const OPENAI_GENERATE_TEXT_PROMPT_EASY = import.meta.env.OPENAI_GENERATE_TEXT_PROMPT_EASY;
+const OPENAI_GENERATE_TEXT_PROMPT_MEDIUM = import.meta.env.OPENAI_GENERATE_TEXT_PROMPT_MEDIUM;
+const OPENAI_GENERATE_TEXT_PROMPT_HARD = import.meta.env.OPENAI_GENERATE_TEXT_PROMPT_HARD;
+
 // The function to translate given text
 export async function generateText(difficulty) {
     difficulty = difficulty.toLowerCase();
@@ -9,4 +13,6 @@ export async function generateText(difficulty) {
     if (!['easy', 'medium', 'hard'].includes(difficulty)) {
         return `Unknown difficulty ${difficulty}! It should be easy, medium, or hard.`;
     }
+
+
 }
