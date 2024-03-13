2ac2f5a3060a9b5f0619c39870a2536af8d882ef
Pawel Michalski <pawel230401@gmail.com>
2024-03-13 16:58:20 -0700

    body added to generateText function

--- fluentify/src/api/openai.js
+++ fluentify/src/api/openai.js
@@ -6,6 +6,12 @@ const OPENAI_GENERATE_TEXT_PROMPT_EASY = import.meta.env.OPENAI_GENERATE_TEXT_PR
 const OPENAI_GENERATE_TEXT_PROMPT_MEDIUM = import.meta.env.OPENAI_GENERATE_TEXT_PROMPT_MEDIUM;
 const OPENAI_GENERATE_TEXT_PROMPT_HARD = import.meta.env.OPENAI_GENERATE_TEXT_PROMPT_HARD;
 
+const prompt = {
+    "easy": OPENAI_GENERATE_TEXT_PROMPT_EASY,
+    "medium": OPENAI_GENERATE_TEXT_PROMPT_MEDIUM,
+    "hard": OPENAI_GENERATE_TEXT_PROMPT_HARD
+}
+
 // The function to translate given text
 export async function generateText(difficulty) {
     difficulty = difficulty.toLowerCase();
@@ -14,5 +20,12 @@ export async function generateText(difficulty) {
         return `Unknown difficulty ${difficulty}! It should be easy, medium, or hard.`;
     }
 
-
+    // Data to be send
+    const data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [{
+            "role": "user",
+            "message": prompt[difficulty]
+        }]
+    }
 }
