f84a91ec18d5b5845c1769bb121f715cab53aedd
Pawel Michalski <pawel230401@gmail.com>
2024-03-13 16:38:27 -0700

    Function defined #4

--- fluentify/src/api/openai.js
+++ fluentify/src/api/openai.js
@@ -1,3 +1,8 @@
 // API info
 const apiUrl = import.meta.env.OPENAI_URL;
 const apiKey = import.meta.env.OPENAI_KEY;
+
+// The function to translate given text
+export async function generateText(difficulty) {
+    
+}
