cb4a965722e9ad4a4e919dd4b0d4376d6e95f185
Pawel Michalski <pawel230401@gmail.com>
2024-03-13 16:42:37 -0700

    Variable validation added to generateText function #4

--- fluentify/src/api/openai.js
+++ fluentify/src/api/openai.js
@@ -4,5 +4,9 @@ const apiKey = import.meta.env.OPENAI_KEY;
 
 // The function to translate given text
 export async function generateText(difficulty) {
-    
+    difficulty = difficulty.toLowerCase();
+
+    if (!['easy', 'medium', 'hard'].includes(difficulty)) {
+        return `Unknown difficulty ${difficulty}! It should be easy, medium, or hard.`;
+    }
 }
