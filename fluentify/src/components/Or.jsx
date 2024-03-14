937ce9dbb00ed4c90415162915867325777670c4
Isabellewn <isabellewn2011@gmail.com>
2024-03-13 17:48:48 -0700

    Display of  main page #3

--- fluentify/src/components/Or.jsx
+++ fluentify/src/components/Or.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Or = () => {
   return (
     <div class="flex items-center justify-center  
-        h-12 w-12 rounded-full bg-blue-600"> 
+        h-12 w-12 rounded-full bg-blue-400"> 
         <span class="text-white font-bold text-2xl"> 
             Or 
         </span> 
