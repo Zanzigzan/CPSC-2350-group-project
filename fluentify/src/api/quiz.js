d745f73b448552c8916db79877267e93a6da9950
Pawel Michalski <pawel230401@gmail.com>
2024-03-22 16:43:22 -0700

    Import of env variables change to accommodate vitest #6

--- fluentify/src/api/quiz.js
+++ fluentify/src/api/quiz.js
@@ -1,5 +1,5 @@
 // API info
-const OPENAI_URL = process.env.OPENAI_URL;
-const OPENAI_KEY = process.env.OPENAI_KEY;
+const OPENAI_URL = import.meta.OPENAI_URL;
+const OPENAI_KEY = import.meta.OPENAI_KEY;
 
-const OPENAI_GENERATE_QUIZ_PROMPT = process.env.OPENAI_GENERATE_QUIZ_PROMPT;
+const OPENAI_GENERATE_QUIZ_PROMPT = import.meta.env.OPENAI_GENERATE_QUIZ_PROMPT;
