55edc1bfb66c278d4d864433483f061e039d630f
Isabellewn <isabellewn2011@gmail.com>
2024-03-21 22:55:22 -0700

    update the animation #51

--- fluentify/src/components/MainHeader.jsx
+++ fluentify/src/components/MainHeader.jsx
@@ -2,29 +2,34 @@ import React from 'react'
 import { useState, useEffect } from 'react';
 
 const MainHeader = () => {
-
-  const Typewriter = ({ text, delay }) => {
+  const Typewriter = ({ text, delay, infinite }) => {
     const [currentText, setCurrentText] = useState('');
     const [currentIndex, setCurrentIndex] = useState(0);
   
-    // Typing logic goes here
     useEffect(() => {
-      const typingInterval = setInterval(() => {
-        setCurrentText(prevText => {
-          const nextIndex = (prevText.length + 1) % text.length;
-          return text.substring(0, nextIndex);
-        });
-      }, delay);
+      let timeout;
   
-      return () => clearInterval(typingInterval);
-    }, [delay, text]);
+      if (currentIndex <= text.length) {
+        timeout = setTimeout(() => {
+          setCurrentText(prevText => prevText + text[currentIndex]);
+          setCurrentIndex(prevIndex => prevIndex + 1);
+        }, delay);
+  
+      } else if (infinite) { // ADD THIS CHECK
+        setCurrentIndex(0);
+        setCurrentText('');
+      }
+  
+      return () => clearTimeout(timeout);
+    }, [currentIndex, delay, infinite, text]);
   
     return <span>{currentText}</span>;
   };
+  
 
   return (
     <div className='bg-blue-400 py-20'>
-        <div className='h-12 mb-8 mt-0 text-white text-5xl font-medium leading-tight text-primary'> <Typewriter text="Find Your Fluent" delay={100} /></div>
+        <div className='h-12 mb-8 mt-0 text-white text-5xl font-medium leading-tight text-primary'> <Typewriter text="Find Your Fluent" delay={150} infinite/></div>
         <div className='text-2xl text-white qleading-tight'>Improve your vocabulary by reading in your target language</div>
     </div>
     
