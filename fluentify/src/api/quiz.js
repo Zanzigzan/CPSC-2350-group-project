ab483dbbb3c0977e73b0ece2d9257f5980462ff5
Pawel Michalski <pawel230401@gmail.com>
2024-03-22 17:00:16 -0700

    Function defined and body of the api call added #6

--- fluentify/src/api/quiz.js
+++ fluentify/src/api/quiz.js
@@ -3,3 +3,14 @@ const OPENAI_URL = import.meta.OPENAI_URL;
 const OPENAI_KEY = import.meta.OPENAI_KEY;
 
 const OPENAI_GENERATE_QUIZ_PROMPT = import.meta.env.OPENAI_GENERATE_QUIZ_PROMPT;
+
+async function generateQuiz(text, detectedSourceLanguage) {
+    // Body
+    const data = {
+        "model": "gpt-3.5-turbo",
+            "messages": [{
+                "role": "user",
+                "content": `OPENAI_GENERATE_QUIZ_PROMPT "${text}"`
+            }]
+    }
+}
