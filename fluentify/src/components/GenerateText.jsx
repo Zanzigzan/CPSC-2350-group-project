2cd3a2c9b66b0a86e19adca87e6a8d60df809ec0
Isabellewn <isabellewn2011@gmail.com>
2024-03-23 23:25:30 -0700

    resolve conflict

--- fluentify/src/components/GenerateText.jsx
+++ fluentify/src/components/GenerateText.jsx
@@ -26,7 +26,7 @@ const GenerateText = (props) => {
         Generate Text
       </div>
       <p className='text-lg'>Choose a level of difficulty</p>
-      <div className='flex text-center space-x-9 border-black border-2 rounded-lg shadow-lg  px-8 py-14'>
+      <div className='flex text-center space-x-9 border-black border-2 rounded-lg shadow-lg px-4 py-14'>
         <button className='bg-green-400 hover:bg-green-700 text-white text-lg font-bold py-2 px-4 rounded' onClick={() => handleClick('easy')}>Easy</button>
         <button className='bg-yellow-400 hover:bg-yellow-700 text-white text-lg  font-bold py-2 px-4 rounded' onClick={() => handleClick('medium')}>Average</button>
         <button className='bg-red-400 hover:bg-red-700 text-white text-lg font-bold py-2 px-4 rounded' onClick={() => handleClick('hard')}>Hard</button>
