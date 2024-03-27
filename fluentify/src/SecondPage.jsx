7c0c14cd0beae27f007be442a4e263ed6bb1e273
stahsin00 <lamisa1998@hotmail.com>
2024-03-26 23:27:36 -0700

    integrate translate function #77

--- fluentify/src/SecondPage.jsx
+++ fluentify/src/SecondPage.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import TranslatedText from "./components/TranslatedText";
 import Logo from "./components/Logo";
 import QuizDisplay from "./components/QuizDisplay";
+import { useLanguage } from "./context/LanguageContext";
+import { translate } from './api/google';
 
 const SecondPage = () => {
+  const [translating, setTranslating] = useState(false);
+  const [error, setError] = useState('');
+  const { language, text, setTranslatedText, setSourceLanguage} = useLanguage();
+  
+  useEffect(() => {
+    TranslateText();
+  }, []);
+
+  async function TranslateText() {
+    if (translating) return;
+    setTranslating(true);
+
+    try {
+      const result = await translate(text, language);
+      setTranslatedText(result.translatedText);
+      setSourceLanguage(result.detectedSourceLanguage);
+    } catch (e) {
+      setError(e.message);
+    } finally {
+      setTranslating(false);
+    }
+
+  }
+
   return (
     <div>
       <div className="flex justify-between items-center pl-8 pr-8">
@@ -22,10 +48,10 @@ const SecondPage = () => {
       </div>
       <div className="flex justify-between mt-8">
         <div className="w-3/4 p-4 px-8 pb-8">
-          <TranslatedText />
+          <TranslatedText translating={translating}/>
         </div>
         <div className="w-1/4 p-4 px-8 pb-8 md:100svh">
-          <QuizDisplay />
+          <QuizDisplay translating={translating}/>
         </div>
       </div>
     </div>
