a05621a1b669bc2e8663704df709f72338df911e
Isabellewn <isabellewn2011@gmail.com>
2024-03-29 16:40:07 -0700

    remove aferEach() in the test.utils file

--- fluentify/src/utils/test.utils.tsx
+++ fluentify/src/utils/test.utils.tsx
@@ -1,9 +1,4 @@
 import { cleanup, render } from "@testing-library/react";
-import { afterEach } from "vitest";
-
-afterEach(() => {
-  cleanup();
-});
 
 function customRender(ui: React.ReactElement, options = {}) {
   return render(ui, {
