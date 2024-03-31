7338018e0c13c7cdad76466993e171749aeee6f0
stahsin00 <lamisa1998@hotmail.com>
2024-03-31 05:46:26 -0700

    modified loading indicator in quiz display #64

--- fluentify/src/components/QuizDisplay.jsx
+++ fluentify/src/components/QuizDisplay.jsx
@@ -73,7 +73,12 @@ export default function QuizDisplay(props) {
         <>
             <div className='col-span-1 bg-blue-400 rounded border border-black text-white p-7 w-full h-full relative min-w-min' >
                 {loading ? 
-                    (<div>Loading...</div>)
+                    (<div className='h-full w-full flex justify-center items-center'>
+                        <svg className='mr-3 h-20 w-20 animate-spin text-white' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24'>
+                            <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' stroke-width="4"></circle>
+                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                        </svg>
+                    </div>)
                     : 
                     (error ? 
                         (<div className='grid h-full'>
