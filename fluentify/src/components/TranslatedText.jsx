15a18e330e6099e16c67bbcdd0465a2bc68ea37d
stahsin00 <lamisa1998@hotmail.com>
2024-03-31 04:25:58 -0700

    added error display on second page #64

--- fluentify/src/components/TranslatedText.jsx
+++ fluentify/src/components/TranslatedText.jsx
@@ -1,14 +1,24 @@
-import React, { useContext } from "react";
+import React from "react";
 import { useLanguage } from "../context/LanguageContext";
+import { Link } from "react-router-dom";
 
-const TranslatedText = () => {
+const TranslatedText = (props) => {
   const { translatedText } = useLanguage();
 
   return (
     <div className="flex">
-      <div className="border-4 border-black rounded-lg h-screen p-4 overflow-auto"> 
-      {/**check if this is right before applying typing effect */}
-        <p>{translatedText}</p>
+      <div className="border-4 border-black rounded-lg h-screen w-full p-4 overflow-auto"> 
+        {
+          props.translating ?
+          <div>Translating...</div>
+          :
+          (
+            props.error ?
+            <div className='text-red-600 text-2xl'>{props.error}... Please go back to <Link to='/' className='font-bold underline hover:text-red-900'>Home Page</Link> to try again.</div>
+            :
+            <p>{translatedText}</p>
+          )
+        }
       </div>
     </div>
   );
