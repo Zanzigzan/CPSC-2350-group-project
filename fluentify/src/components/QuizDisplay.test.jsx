f12bacc74e7612888c0a51cb27e30227fe7155d8
stahsin00 <lamisa1998@hotmail.com>
2024-03-30 23:23:29 -0700

    added integration test for error case of quiz generation #128

--- fluentify/src/components/QuizDisplay.test.jsx
+++ fluentify/src/components/QuizDisplay.test.jsx
@@ -1,7 +1,7 @@
 import QuizDisplay from './QuizDisplay';
 import { useLanguage } from '../context/LanguageContext.jsx'
 import { describe, expect, it, vi } from 'vitest';
-import {render, screen} from '@testing-library/react'
+import {render, screen, fireEvent} from '@testing-library/react'
 
 vi.mock('../context/LanguageContext.jsx', () => ({
     useLanguage: vi.fn(),
@@ -13,7 +13,53 @@ useLanguage.mockReturnValue({
 });
 
 describe('generate quiz', () => {
-    it('should display error when an error is encountered', async () => {
+
+    it('should display quiz question and options in quiz display', async () => {
+        global.fetch = vi.fn()
+            .mockReturnValueOnce(Promise.resolve({
+                ok: true,
+                json: () => Promise.resolve({
+                    choices: [
+                        {
+                            message: {
+                                content: "[\"test option a\", \"test option b\", \"test option c\", \"test option d\"]"
+                            }
+                        }
+                    ]
+                })
+            }))
+            .mockReturnValueOnce(Promise.resolve({
+                ok: true,
+                json: () => Promise.resolve({
+                    data: {
+                        translations: [
+                            {
+                                detectedSourceLanguage: "de",
+                                translatedText: "test question"
+                            }
+                        ]
+                    }
+                })
+            }));
+
+        render(<QuizDisplay translating={false}/>);
+
+        const textContent = ['test question', 'test option a', 'test option b', 'test option c', 'test option d'];
+
+        for (const text of textContent) {
+            expect(screen.findByText(text)).resolves.toBeInTheDocument();
+        }
+
+        const nextButton = await screen.findByText('Next');
+        expect(nextButton).toBeInTheDocument();
+
+        expect(nextButton).toHaveClass('invisible');
+        fireEvent.click(await screen.findByText('test option a'));
+        expect(nextButton).toHaveClass('visible');
+    });
+
+
+    it('should display error when an error is encountered while generating quiz', async () => {
         global.fetch = vi.fn(() =>
             Promise.resolve({
                 ok: false,
@@ -25,7 +71,7 @@ describe('generate quiz', () => {
 
         render(<QuizDisplay translating={false}/>);
 
-        await expect(screen.findByText('HTTP error! Status: 500')).resolves.toBeInTheDocument();
-        await expect(screen.findByText('Try again.')).resolves.toBeInTheDocument();
+        expect(screen.findByText('HTTP error! Status: 500')).resolves.toBeInTheDocument();
+        expect(screen.findByText('Try again.')).resolves.toBeInTheDocument();
     });
 });
