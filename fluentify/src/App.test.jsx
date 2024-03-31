6b1137e84de189abe7f2f679cae899826b41deb1
Van Valdez <valdez.vanms@gmail.com>
2024-03-31 13:05:00 -0700

    add test for page 1 #127

--- fluentify/src/App.test.jsx
+++ fluentify/src/App.test.jsx
@@ -1,28 +1,23 @@
 import { vi } from "vitest";
 import { render, screen } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
-import SecondPage from "./SecondPage";
-import { LanguageProvider } from "./context/LanguageContext";
-import * as LanguageContextModule from "./context/LanguageContext";
-import * as googleApi from "./api/google";
+import App from "./App";
 
-describe("SecondPage component", () => {
-    afterEach(() => {
-      vi.restoreAllMocks();
-    });
-  
-    //rendering main page
-    it("renders without crashing", () => {
-      render(
-        <LanguageProvider>
-          <MemoryRouter>
-            <SecondPage />
-          </MemoryRouter>
-        </LanguageProvider>,
-      );
+describe("Main Page", () => {  
+    //file has more than 10 words
+    it("allows user upload for files with more than 10 words", () => {
+        render(<App />);
+    
+        const file = new File(["Hello World"], "hello.txt", { type: "text/plain" });
+        const input = screen.getByLabelText("Select a file");
+        const submitButton = screen.getByText("Submit");
+    
+        vi(input).fire.change(file);
+        vi(submitButton).fire.click();
+    
+        expect(screen.getByText("Hello World")).toBeInTheDocument();
     });
     
-    
+
     it("renders translated text", async () => {
       render(
         <LanguageProvider>
