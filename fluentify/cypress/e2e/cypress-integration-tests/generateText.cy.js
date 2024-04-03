a5932dd5e50a67286b0f190661c18a1379125891
Isabellewn <isabellewn2011@gmail.com>
2024-04-03 16:20:47 -0700

    generate text feature tests, 3 scenarios #122

---
+++ fluentify/cypress/e2e/cypress-integration-tests/generateText.cy.js
@@ -0,0 +1,29 @@
+describe("Generate Text Feature", ()=>{
+  //Test 1
+  it('click the easy button, expect the modal with language selection shows up', ()=>{
+      cy.visit('localhost:5173/')
+      cy.get('.bg-green-400').click()
+      cy.wait(6000)
+      cy.get('.bg-black').should('be.visible')
+      cy.get('h1.text-white.text-xl.font-bold').should('have.text', 'Please choose your target language:')
+  })
+
+  //test 2
+  it('click the medium button, expect the modal with language selection shows up', ()=>{
+    cy.visit('localhost:5173/')
+    cy.get('.bg-yellow-400').click()
+    cy.wait(6000)
+    cy.get('.bg-black').should('be.visible')
+    cy.get('h1.text-white.text-xl.font-bold').should('have.text', 'Please choose your target language:')
+  })
+  
+  //test 3
+  it('click the hard button, expect the modal with language selection shows up', ()=>{
+      cy.visit('localhost:5173/')
+      cy.get('.bg-green-400').click()
+      cy.wait(6000)
+      cy.get('.bg-black').should('be.visible')
+      cy.get('h1.text-white.text-xl.font-bold').should('have.text', 'Please choose your target language:')
+  })
+
+})
