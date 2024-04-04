60af09ec08fd3edad119892f3a2fe7b49bdfe907
Isabellewn <isabellewn2011@gmail.com>
2024-04-03 17:13:22 -0700

    second test for translate feature #145

--- fluentify/cypress/e2e/cypress-integration-tests/translateFeature.cy.js
+++ fluentify/cypress/e2e/cypress-integration-tests/translateFeature.cy.js
@@ -1,6 +1,6 @@
 describe("Translate Feature", ()=>{
     //Test 1
-    it('choose a language and click the confirm button, expect the translated text to show up on 2nd page', ()=>{
+    it.skip('choose a language and click the confirm button, expect the translated text to show up on 2nd page', ()=>{
         cy.visit('localhost:5173/')
         cy.get('.bg-yellow-400').click()
         cy.wait(6000)
@@ -11,5 +11,14 @@ describe("Translate Feature", ()=>{
         cy.get('p').should('exist')
     })
 
- 
+    //Test 2
+    it('choose a language and click the back button, expect the user to continue stay on 1st page', ()=>{
+        cy.visit('localhost:5173/')
+        cy.get('.bg-yellow-400').click()
+        cy.wait(6000)
+        cy.get('h1.text-white.text-xl.font-bold').should('have.text', 'Please choose your target language:')
+        cy.get('select').select('fr')
+        cy.get('button.bg-gray-500').click()
+        cy.get('p.text-lg').contains('Choose a level of difficulty')
+    })
 })    
