44d31d434d2217b96364766cf42bdaf5e1b97be3
Isabellewn <isabellewn2011@gmail.com>
2024-04-03 17:45:29 -0700

    fix button overflow on generate text part #126

--- fluentify/src/components/GenerateText.jsx
+++ fluentify/src/components/GenerateText.jsx
@@ -31,7 +31,7 @@ const GenerateText = (props) => {
         Generate Text
       </div>
       <p className='text-lg'>Choose a level of difficulty</p>
-      <div className='flex text-center justify-center space-x-9 border-black border-2 rounded-lg shadow-lg px-4 py-14 relative'>
+      <div className='flex text-center justify-center space-x-7 border-black border-2 rounded-lg shadow-lg px-4 py-14 overflow-auto'>
         <button className='bg-green-400 hover:bg-green-700 text-white text-lg font-bold py-2 px-4 rounded' onClick={() => handleClick('easy')}>Easy</button>
         <button className='bg-yellow-400 hover:bg-yellow-700 text-white text-lg  font-bold py-2 px-4 rounded' onClick={() => handleClick('medium')}>Average</button>
         <button className='bg-red-400 hover:bg-red-700 text-white text-lg font-bold py-2 px-4 rounded' onClick={() => handleClick('hard')}>Hard</button>
