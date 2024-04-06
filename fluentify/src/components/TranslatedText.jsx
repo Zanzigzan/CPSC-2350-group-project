06a7ba26eca17689b6dfec2df8d3f9a73c482046
Van Valdez <valdez.vanms@gmail.com>
2024-04-05 20:28:31 -0700

    fixed merge conflicts

--- fluentify/src/components/TranslatedText.jsx
+++ fluentify/src/components/TranslatedText.jsx
@@ -32,11 +32,7 @@ const TranslatedText = (props) => {
 
   return (
     <div className="flex">
-<<<<<<< HEAD
       <div className="font-museo border-4 border-gray-700 border-opacity-90 rounded-lg h-screen w-full overflow-auto text-xl p-7 text-left"> 
-=======
-      <div className="font-museo border-4 border-black rounded-lg overflow-auto text-lg p-8"> 
->>>>>>> main
         {
           props.translating ?
           <div className='italic'><TextAnimation /></div>
