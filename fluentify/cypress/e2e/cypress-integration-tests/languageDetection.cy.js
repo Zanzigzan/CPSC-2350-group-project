d13379328978ed1532ca3252c06fc2bc68349d9c
Isabellewn <isabellewn2011@gmail.com>
2024-04-08 21:21:33 -0700

    first test case #166

--- fluentify/cypress/e2e/cypress-integration-tests/languageDetection.cy.js
+++ fluentify/cypress/e2e/cypress-integration-tests/languageDetection.cy.js
@@ -1,20 +1,28 @@
 describe("Language Detection Feature", ()=>{
     //Test 1
-    it('Upload a file using supported language, should show language detection result', ()=>{
+    it.skip('Upload a file using supported language, should show language detection result', ()=>{
         cy.visit('https://fluent-ify.netlify.app/')
 
-        cy.get('label.h-40').as('fileInput')
+        const fileName = 'test file - fr.txt'
 
-        cy.fixture('test file - fr.txt').then(fileContent => {
-            cy.get('@fileInput').attachFile({
+        cy.get('input[type="file"]').invoke('removeClass', 'hidden')
+
+        cy.fixture(fileName).then(fileContent => {
+            cy.get('input[type="file"]').attachFile({
             fileContent: fileContent.toString(),
             fileName: 'test file - fr.txt',
-            mimeType: 'txt'
+            mimeType: 'text'
             });
-            });
+        });
 
-
+        cy.get('.bg-grey-lighter > .text-blue-400').contains('Detected Language: French')
     })
+
+    //Test 2
+    it('Upload a file using unsupported language, should show a reminder', ()=>{
+      
+    })
+
   
   
   })
