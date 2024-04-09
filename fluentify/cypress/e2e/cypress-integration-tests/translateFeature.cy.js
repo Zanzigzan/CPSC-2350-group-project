2024d2dc7b9a6d74721f2ecb117d650a8a3bb1e5
Isabellewn <isabellewn2011@gmail.com>
2024-04-08 17:42:30 -0700

    changed selectors for test 2

--- fluentify/cypress/e2e/cypress-integration-tests/translateFeature.cy.js
+++ fluentify/cypress/e2e/cypress-integration-tests/translateFeature.cy.js
@@ -1,6 +1,6 @@
 describe("Translate Feature", ()=>{
     //Test 1
-    it.skip('choose a language and click the confirm button, expect the translated text to show up on 2nd page', ()=>{
+    it('choose a language and click the confirm button, expect the translated text to show up on 2nd page', ()=>{
         cy.visit('https://fluent-ify.netlify.app/')
         cy.get('.bg-yellow-400').click()
         cy.wait(6000)
@@ -18,7 +18,7 @@ describe("Translate Feature", ()=>{
         cy.wait(6000)
         cy.get('h1.text-white.text-xl.font-bold').should('have.text', 'Please choose your target language:')
         cy.get('select').select('fr')
-        cy.get('button.bg-gray-500').click()
-        cy.get('p.text-lg').contains('Choose a level of difficulty')
+        cy.get('.flex > .bg-gray-500').click()
+        cy.get('div.text-lg').contains('Choose a level of difficulty')
     })
 })    
